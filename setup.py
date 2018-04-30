5f452567a10d5b318fe837604d0a09db4567a75c
Dinu Gherman <gherman@darwin.in-berlin.de>
2018-05-01 00:44:46 +0200

    Fix wrong version string

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/deeplook/asciinemaster',
-    version='version='version='0.2.0''',
+    version='0.2.0',
     zip_safe=False,
 )
